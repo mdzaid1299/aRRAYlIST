c75380b98e2f05cecaad6159c9e7d32a002a3718
HP <mdzaid1299@gmail.com>
2022-09-02 23:26:05 +0530

    initial

--- src/StudentSort.java
+++ src/StudentSort.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class StudentSort {
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class StudentSort {
+    public static void main(String[] args) {
+        List<Integer> studentSort = Arrays.asList(7,3,2,8,6,9,1,3);
+        System.out.println(studentSort);
+        Collections.sort(studentSort);
+        System.out.println(studentSort);
+    }
 }
+
